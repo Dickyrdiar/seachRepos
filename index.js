3517d82f1d4b2491b8196211cd532d2c388cf3e8
Dicky Ardiar <dickyardiar1@gmail.com>
2022-05-20 11:31:33 +0700

    commit search repos

---
+++ index.js
@@ -0,0 +1,34 @@
+const axios = require('axios')
+
+const searchRepo = async ({ 
+  username,
+  page,
+  per_page
+} = {}) => {
+  try {
+    const repo = await axios.get(
+      `https://api.github.com/users/${username}/repos?page=${page}&per_page=${per_page}&sort=updated`
+    )
+    
+    return repo?.data?.map((value) => {
+      return {
+        name: value.name,
+        link: value.html_url, 
+        description: value.description,
+        stars: value.stargazers_count, 
+        watch: value.watchers_count,
+        language: value.language
+      }
+    })
+    .sort((first, second) => first.start - second.start)
+
+  } catch(error) {
+    return []
+  }
+};
+
+searchRepo({
+  username: 'Dickyrdiar',
+  page: 1,
+  per_page: 20
+}).then((repositories) => console.log(repositories));
